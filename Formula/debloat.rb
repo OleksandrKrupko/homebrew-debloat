fccefa6b181d52fb07c258590cb520f4f0de6bdc
OleksandrKrupko <oleksandr.krupko@corva.ai>
2026-06-09 21:29:04 +0200

    Bump to 0.1.3

--- Formula/debloat.rb
+++ Formula/debloat.rb
@@ -1,10 +1,10 @@
 class Debloat < Formula
   desc "Interactive console util to toggle macOS launchd services (frees RAM)"
   homepage "https://github.com/OleksandrKrupko/mac-os-debloat"
-  url "https://github.com/OleksandrKrupko/mac-os-debloat/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "6f61f02c8b8c733e04f717029772b195335fe636d3a6eb52a66ac262ae6534d1"
+  url "https://github.com/OleksandrKrupko/mac-os-debloat/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "2ad7424f51f7d6073c2a78a96a86f693638ee1f04286f54ef2eb4f8d58dd22d2"
   license "MIT"
-  version "0.1.2"
+  version "0.1.3"
 
   depends_on :macos
 
@@ -13,6 +13,6 @@ class Debloat < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/debloat --version")
+    assert_match "0.1.3", shell_output("#{bin}/debloat --version")
   end
 end

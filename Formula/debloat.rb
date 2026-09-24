fc6656413dacc2bf15f316e269a5caf7bb9cba35
Oleksandr Krupko <oleksandr.krupko@corva.ai>
2026-09-24 17:40:11 +0200

    debloat 0.12.2

--- Formula/debloat.rb
+++ Formula/debloat.rb
@@ -1,10 +1,10 @@
 class Debloat < Formula
   desc "Console util to toggle macOS launchd services and the Spotlight index (frees RAM)"
   homepage "https://github.com/OleksandrKrupko/mac-os-debloat"
-  url "https://github.com/OleksandrKrupko/mac-os-debloat/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "58672f14aafe64d31f2f4851bfe6af6a8fbbf8a91fd105f03b8f2cb96619c375"
+  url "https://github.com/OleksandrKrupko/mac-os-debloat/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "458c1e2111d25422b685dfadd9ce70f77a21ee808d735a50dbb5931c9e69baf5"
   license "MIT"
-  version "0.12.1"
+  version "0.12.2"
 
   depends_on :macos
 
@@ -13,6 +13,6 @@ class Debloat < Formula
   end
 
   test do
-    assert_match "0.12.1", shell_output("#{bin}/debloat --version")
+    assert_match "0.12.2", shell_output("#{bin}/debloat --version")
   end
 end
